427b28549e1de63ae49e57330c7c74e10dc70646
fzliang <cnfzliang@gmail.com>
2022-07-26 21:42:52 +0800

    first commit

---
+++ PCR/ContentView.swift
@@ -0,0 +1,149 @@
+//
+//  ContentView.swift
+//  PCR
+//
+//  Created by fengzhiliang on 2022/7/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    private var currentNotificationCenter = UNUserNotificationCenter.current()
+    @State private var hint:String = "明天"
+    @State private var nextPRCDate:DateComponents = Calendar.current.dateComponents(in: TimeZone.current, from: Date())
+    
+    
+    var body: some View {
+        VStack {
+            Text("你 \(hint) 该去做核酸了").fontWeight(.bold).font(.title).foregroundColor(.red)
+            
+            Text("下次做核酸的日期👉\(nextPRCDate.month!)月\(nextPRCDate.day!)日👈").foregroundColor(.gray)
+            
+            Button("😎 我今天做了") {
+                onTap(days: 0)
+            }.buttonStyle(.borderedProminent).padding(.top, 10)
+            
+            Button("🤔 我昨天做了") {
+                onTap(days: 1)
+            }.buttonStyle(.borderedProminent).padding(.top, 10)
+            
+            Button("😷 我前天做了") {
+                onTap(days: 2)
+            }.buttonStyle(.borderedProminent).padding(.top, 10)
+            
+            Button("😮‍💨 别找了你今天该去了") {
+                onTap(days: 3)
+            }.foregroundColor(.red).padding(.top, 10)
+            
+            Button("😓 要不还是去健康宝看看吧") {
+                let urlStr = "alipays://platformapi/startapp?appId=2021001135679870".addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)
+                
+                if let url = URL(string: urlStr!) {
+                    UIApplication.shared.open(url)
+                }
+                
+            }.padding(.top, 5)
+        }.onAppear() {
+            resetNextPRCDate()
+        }
+    }
+    
+    func resetNextPRCDate() {
+        currentNotificationCenter.getPendingNotificationRequests { (requests) in
+            for request in requests {
+                print(request)
+                if let trigger  = request.trigger as? UNCalendarNotificationTrigger {
+                    nextPRCDate = trigger.dateComponents
+                    
+                    let today = getDateComponentsAfterDays(days: 0);
+                    if (today.month == nextPRCDate.month
+                        && today.day == nextPRCDate.day) {
+                        hint = "今天"
+                        return
+                    }
+                    let tomorrow = getDateComponentsAfterDays(days: 1);
+                    if (tomorrow.month == nextPRCDate.month
+                        && tomorrow.day == nextPRCDate.day) {
+                        hint = "明天"
+                        return
+                    }
+                    let theDayAfterTomorrow = getDateComponentsAfterDays(days: 2);
+                    if (theDayAfterTomorrow.month == nextPRCDate.month
+                        && theDayAfterTomorrow.day == nextPRCDate.day) {
+                        hint = "后天"
+                        return
+                    }
+                    hint = "\(nextPRCDate.month!)月\(nextPRCDate.day!)日"
+                    return
+                }
+            }
+        }
+    }
+    
+    func getDateComponentsAfterDays(days: Int) -> DateComponents {
+        var newDate = Calendar.current.date(byAdding: .day, value: days, to: Date())
+        newDate = Calendar.current.date(bySettingHour: 8, minute: 0, second: 0, of: newDate!)
+        
+        let date = Calendar.current.dateComponents([.day, .month, .year, .hour, .minute], from: newDate!)
+        
+        return date
+    }
+    
+    func onTap(days: Int) {
+        if (days == 0) {
+            clearNotify()
+            notify(days: 3, hour: 9, minute: 0)
+            notify(days: 3, hour: 17, minute: 30)
+        } else if (days == 1) {
+            clearNotify()
+            notify(days: 2, hour: 9, minute: 0)
+            notify(days: 2, hour: 17, minute: 30)
+        } else if (days == 2) {
+            clearNotify()
+            notify(days: 1,hour: 9, minute: 0)
+            notify(days: 1,hour: 17, minute: 30)
+        } else if (days == 3) {
+            clearNotify()
+            notify(days: 0, hour: 9, minute:  0)
+            notify(days: 0, hour: 17, minute: 30)
+        }
+    }
+    
+    func clearNotify() {
+        currentNotificationCenter.removeAllPendingNotificationRequests();
+        currentNotificationCenter.removeAllDeliveredNotifications();
+    }
+    
+    func notify(days: Int, hour: Int, minute: Int) {
+       
+        var newDate = Calendar.current.date(byAdding: .day, value: days, to: Date())
+        newDate = Calendar.current.date(bySettingHour: hour, minute: minute, second: 0, of: newDate!)
+        
+        let date = Calendar.current.dateComponents([.day, .month, .year, .hour, .minute], from: newDate!)
+        
+        let content = UNMutableNotificationContent()
+        content.title = "做核酸了"
+        content.body = "今天\(date.month!)月\(date.day!)日,该去做核酸了！！！"
+        content.sound = .default
+        
+        let trigger = UNCalendarNotificationTrigger(dateMatching: date, repeats: true)
+        
+        let identifier = "pcr \(hour) \(minute)"
+        
+        let request = UNNotificationRequest(identifier: identifier, content: content, trigger: trigger)
+        
+        currentNotificationCenter.add(request) { error in
+            if let error = error {
+                print(error)
+            }
+            resetNextPRCDate()
+        }
+    }
+    
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PCR/PCRApp.swift
@@ -0,0 +1,45 @@
+//
+//  PCRApp.swift
+//  PCR
+//
+//  Created by fengzhiliang on 2022/7/24.
+//
+
+import SwiftUI
+import UserNotifications
+
+@main
+struct PCRApp: App {
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        UNUserNotificationCenter.current().delegate = self
+        let center = UNUserNotificationCenter.current()
+        center.requestAuthorization(options: [.alert, .sound, .badge]) { granted, error in
+            if let error = error {
+                print(error)
+            }
+        }
+        return true;
+    }
+}
+
+extension AppDelegate: UNUserNotificationCenterDelegate {
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler([.alert, .badge, .sound])
+    }
+
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        completionHandler()
+    }
+}
